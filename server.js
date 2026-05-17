d41ba560c374fbf826db9099f31aa63143403699
Facundo598 <103714602+Facundo598@users.noreply.github.com>
2026-05-16 21:49:06 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,14 +6,39 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const GOOGLE_SCRIPT_URL = "PEGAR_URL_APPS_SCRIPT";
+
 app.post("/guardar", async (req, res) => {
 
-    console.log("Datos recibidos:", req.body);
+    try {
 
-    res.json({
-        ok: true,
-        mensaje: "Datos recibidos correctamente"
-    });
+        const respuesta = await fetch(GOOGLE_SCRIPT_URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(req.body)
+        });
+
+        const texto = await respuesta.text();
+
+        console.log("Respuesta Google:", texto);
+
+        res.json({
+            ok: true,
+            mensaje: "Datos guardados"
+        });
+
+    } catch (error) {
+
+        console.error(error);
+
+        res.status(500).json({
+            ok: false,
+            mensaje: "Error"
+        });
+
+    }
 
 });
 
